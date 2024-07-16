b5813d120c55c1141929bf602fd58182532654be
LEONARDO MICELLI <leonardomicelli@gmail.com>
2024-07-16 22:14:04 +0200

    feat: add simple health management

---
+++ scripts/health_component.gd
@@ -0,0 +1,22 @@
+@tool class_name HealthComponent extends Node2D
+
+signal health_depleted
+@export var health: int = 100
+@onready var health_bar = $HealthBar
+
+func set_health(new_amount: int):
+	health = new_amount
+	health_bar.value = health
+	if health == 0:
+		health_depleted.emit()
+	
+func gain_life(amount: int):
+	set_health(health + amount)
+	
+func take_damage(amount: int):
+	set_health(health - amount)
+	print("You took\t" + str(amount) + "\tdamage! Accura Picciott")
+
+func _ready():
+	health_bar.max_value = health
+	health_bar.value = health
