382d8bad90e50bcc70a8421e8f298127236025b1
LEONARDO MICELLI <leonardomicelli@gmail.com>
2024-07-20 15:55:54 +0200

    feat: start spawning logic

---
+++ scripts/enemy_spawner.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+@export var spawn_infos: Array[SpawnInfo] = []
+var time = 0
+
+func _on_timer_timeout():
+	time += 1
+	for si in spawn_infos:
+		if time >= si.time_start and time <= si.time_end:
+			if si.spawn_delay_counter <= si.spawn_delay:
+				si.spawn_delay_counter += 1
+			else:
+				si.spawn_delay_counter = 0
+				var count = 0
+				while count < si.enemy_number:
+					var enemy = si.enemy_res.instantiate()
+					enemy.global_position = get_random_position()
+					add_child(enemy)
+					count += 1
+
+func get_random_position() -> Vector2:
+	return Vector2.ZERO
