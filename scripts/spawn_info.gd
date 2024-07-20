daecb26b3564f90197b95d72f0db59ef54112196
LEONARDO MICELLI <leonardomicelli@gmail.com>
2024-07-20 15:42:42 +0200

    feat: create spawn info

---
+++ scripts/spawn_info.gd
@@ -0,0 +1,8 @@
+class_name SpawnInfo extends Resource
+
+@export var time_start: int
+@export var time_end: int
+@export var enemy_res: Resource
+@export var enemy_number: int
+@export var spawn_delay: int
+
