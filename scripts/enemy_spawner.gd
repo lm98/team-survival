dcb5ff7e008c47ef235179ca754306fba65e8d59
LEONARDO MICELLI <leonardomicelli@gmail.com>
2024-07-21 11:12:48 +0200

    feat: add wave enemy spawner

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var spawn_infos: Array[SpawnInfo] = []
+@onready var spawn_position_pointer = %SpawnPositionPointer
 var time = 0
 
 func _on_timer_timeout():
@@ -13,10 +14,12 @@ func _on_timer_timeout():
 				si.spawn_delay_counter = 0
 				var count = 0
 				while count < si.enemy_number:
+					print("Spawning Enemy")
 					var enemy = si.enemy_res.instantiate()
 					enemy.global_position = get_random_position()
 					add_child(enemy)
 					count += 1
 
 func get_random_position() -> Vector2:
-	return Vector2.ZERO
+	spawn_position_pointer.progress_ratio = randf()
+	return spawn_position_pointer.global_position
