1c906294421f35f623e15aae0c493aa1862c8f83
LEONARDO MICELLI <leonardomicelli@gmail.com>
2024-07-22 19:21:39 +0200

    feat: add skeleton enemy and knight player

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -3,6 +3,7 @@ class_name Enemy extends CharacterBody2D
 @export var speed: int = 20
 @onready var player_team: PlayerTeam = get_node("/root/Game/PlayerTeam")
 @onready var health_component = $HealthComponent
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(_delta):
 	var targets = player_team.get_members()
@@ -16,6 +17,12 @@ func _physics_process(_delta):
 	
 	var direction = global_position.direction_to(current_target)
 	velocity = direction * speed
+	
+	if velocity.length() > 0.0:
+		animated_sprite.play("move")
+	else:
+		animated_sprite.play("idle")
+
 	move_and_slide()
 
 func _on_health_component_health_depleted():
