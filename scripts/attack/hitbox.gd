3d95562f5a551973a8b89584e96721bf25cbe2de
LEONARDO MICELLI <leonardomicelli@gmail.com>
2024-07-29 16:00:16 +0200

    feat: add hitbox and hurtbox

--- scripts/attack/hitbox.gd
+++
@@ -1,7 +0,0 @@
-class_name Hitbox extends Area2D
-
-signal enemy_entered(enemy)
-
-func _on_area_entered(area):
-	print("enemy in range!")
-	enemy_entered.emit(area)
---
+++ scripts/attack/hurt_box.gd
@@ -0,0 +1,12 @@
+class_name HurtBox extends Area2D
+
+func _init():
+	collision_layer = 0
+	collision_mask = 2
+
+func _on_area_entered(hitbox: HitBox):
+	if hitbox == null:
+		return
+
+	if owner.has_method("take_damage"):
+		owner.take_damage(hitbox.damage)
