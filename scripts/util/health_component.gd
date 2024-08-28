bca4036ab48d96e161de2e3ff1f357ae3f3aa02e
LEONARDO MICELLI <leonardomicelli@gmail.com>
2024-08-28 23:03:28 +0200

    refactor: make health component manage hurtbox

--- scripts/util/health_component.gd
+++ scripts/util/health_component.gd
@@ -4,10 +4,14 @@ signal health_depleted
 @export var health: int = 100
 @onready var health_bar = $HealthBar
 
+func _physics_process(delta: float) -> void:
+	if health <= 0:
+		health_depleted.emit()
+
 func set_health(new_amount: int):
 	health = new_amount
 	health_bar.value = health
-	if health == 0:
+	if health <= 0:
 		health_depleted.emit()
 	
 func gain_life(amount: int):
