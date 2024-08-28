1609549766ac89d6bb325af5eeb4f5f5362e4e49
LEONARDO MICELLI <leonardomicelli@gmail.com>
2024-08-28 23:27:18 +0200

    feat: add iframes after being hurt

--- scripts/attack/energy_ball_big.gd
+++ scripts/attack/energy_ball_big.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @export var speed: int = 80
 @export var range: int = 100
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var hitbox: HitBox = $Hitbox
 
 var travelled_distance = 0
 
@@ -16,3 +17,9 @@ func _physics_process(delta):
 
 	if travelled_distance > range:
 		queue_free()
+
+
+func _on_hitbox_area_entered(hurtbox: HurtBox) -> void:
+	if hurtbox == null:
+		return
+	hitbox.disable_mode
