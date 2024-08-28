5758edbc5531d430f772d6ffa5ac2a3a0a451608
LEONARDO MICELLI <leonardomicelli@gmail.com>
2024-08-28 23:27:18 +0200

    feat: add iframes after being hurt

--- scripts/attack/hurt_box.gd
+++ scripts/attack/hurt_box.gd
@@ -1,8 +1,17 @@
 class_name HurtBox extends Area2D
 
-func _init():
-	collision_layer = 3
-	collision_mask = 2
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
+@onready var disable_timer: Timer = $DisableTimer
+
+
+func _physics_process(delta: float) -> void:
+	var damages: Array = get_overlapping_areas().map(func(hb: HitBox): return hb.damage)
+	if not damages.is_empty():
+		if owner.has_method("take_damage"):
+			for dmg in damages:
+				owner.take_damage(dmg)
+			collision_shape.set_deferred("disabled", true)
+			disable_timer.start()
 
 func _on_area_entered(hitbox: HitBox):
 	if hitbox == null:
@@ -10,3 +19,6 @@ func _on_area_entered(hitbox: HitBox):
 
 	if owner.has_method("take_damage"):
 		owner.take_damage(hitbox.damage)
+
+func _on_disable_timer_timeout() -> void:
+	collision_shape.set_deferred("disabled", false)
