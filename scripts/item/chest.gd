76c61cb1224f2c89f33faca975ef73ba8a47a4b1
LEONARDO MICELLI <leonardomicelli@gmail.com>
2024-09-17 23:31:20 +0200

    refactor: use resources to represent player characters

--- scripts/item/chest.gd
+++ scripts/item/chest.gd
@@ -1,26 +1,18 @@
 @tool class_name Chest extends Area2D
 
-enum ChestType {Random, PlayerChoice}
-
 @export var price_to_open: int = 0
-@export var chest_type: ChestType
+@export var content: PlayerCharacter
+
 @onready var label: Label = $Label
 @onready var pm: ProgressionManager = get_node("/root/Game/ProgressionManager")
 @onready var pt: PlayerTeam = get_node("/root/Game/PlayerTeam")
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-
-var player_characters = ["Knight", "FighterRed"]
-var content: String
 
 func _ready():
 	if pm == null:
 		printerr("No Progression Manager!")
 	elif pt == null:
 		printerr("No Player Team!")
-	match chest_type:
-		ChestType.Random:
-			content = player_characters.pick_random()
-		_: pass
 
 func _physics_process(delta: float) -> void:
 	label.text = str(price_to_open)
--- scripts/player/player_team.gd
+++ scripts/player/player_team.gd
@@ -1,14 +1,11 @@
 class_name PlayerTeam extends Area2D
 
-const team_members = {
-	"Knight": preload("res://scenes/player/knight.tscn"),
-	"FighterRed": preload("res://scenes/player/fighter_red.tscn")
-}
+@export var team_members: Array[PlayerCharacter]
 
 var members: Array[TeamMember] = []
 
 func _ready():
-	spawn_new_member(team_members.values().pick_random(), global_position)
+	spawn_new_member(team_members.pick_random(), global_position)
 
 func _physics_process(_delta: float):
 	if Input.is_action_just_pressed("left_mouse_click"):
@@ -18,8 +15,8 @@ func _physics_process(_delta: float):
 func get_members() -> Array:
 	return Array(members)
 
-func spawn_new_member(member_res: Resource, pos: Vector2):
-	var member = member_res.instantiate()
+func spawn_new_member(member_res: PlayerCharacter, pos: Vector2):
+	var member = member_res.scene.instantiate()
 	member.connect("member_died", on_member_death)
 	add_child(member)
 	member.global_position = pos
@@ -32,5 +29,5 @@ func on_member_death(member: TeamMember):
 	remove_member(member)
 	pass
 
-func on_chest_open(member: String, position: Vector2) -> void:
-	spawn_new_member(team_members[member], position)
+func on_chest_open(member: PlayerCharacter, position: Vector2) -> void:
+	spawn_new_member(member, position)
